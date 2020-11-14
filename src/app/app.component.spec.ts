1d030f02800594e4e8a0e803afbde08b741f58d2
Prapulla1359 <prapulla1359@gmail.com>
2020-11-14 23:40:43 +0530

    Added for Angular implementation of Mortgage Calculator :Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mortage-calculator'`, () => {
+  it(`should have as title 'mortgage-calculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mortage-calculator');
+    expect(app.title).toEqual('mortgage-calculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mortage-calculator app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mortgage-calculator app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ChartModule } from 'primeng/chart';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputComponent } from './output/output.component';
+import { InputComponent } from './input/input.component';
+import {MortgageService} from './service/mortgage.service';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputComponent,
+    InputComponent,
+    HeaderComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ChartModule
   ],
-  providers: [],
+  providers: [MortgageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
