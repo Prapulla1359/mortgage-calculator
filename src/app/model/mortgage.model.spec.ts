a4b05d8d281240dbb2d2ac5c9addb6f8dca5d9fd
Prapulla1359 <prapulla1359@gmail.com>
2020-11-16 14:48:46 +0530

    updated spec file for model.

--- src/app/model/mortgage.model.spec.ts
+++ src/app/model/mortgage.model.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MortgageService } from './mortgage.service';
+import { MortgageService } from '../service/mortgage.service';
 
 describe('MortgageService', () => {
   let service: MortgageService;
